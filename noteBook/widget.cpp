8a283107665de696584b084af3e00b8eac033521
Mms <jsnjcbb2001@163.com>
2025-04-18 00:04:02 +0800

    更新了ui显示图标

--- noteBook/widget.cpp
+++ noteBook/widget.cpp
@@ -6,6 +6,10 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    //虽然上面一行代码进行widet和ui窗口关联，但是如何发送窗口大小变化的时候，里面的布局不会随之变化
+    //通过下面这行代码进行显示说明，让窗口变化时，布局及其子控件随之调整
+    this->setLayout(ui->verticalLayout);
+    ui->widgetBottom->setLayout(ui->horizontalLayout);
 }
 
 Widget::~Widget()
